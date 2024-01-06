10409d01f8fe47eea336e9bb93adef3e2d56e498
Shcoobz <a.boeckmann23@gmail.com>
2024-01-06 18:48:30 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-01447f14853a6cac65d8.js"
+    "url": "webpack-runtime-a4583c9ece8976603899.js"
   },
   {
     "url": "framework-524a304121a1bdc94522.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "709ba393fb2e35ff234a66b380954087"
+    "revision": "3db44f70bd1ddd0cec2cd256cdcd97f1"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c33574fbc14be9513f80.js"
